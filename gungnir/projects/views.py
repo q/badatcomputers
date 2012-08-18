0c26fcbbcf4b28a27859e2e49ce776f409611edb
Tyler McMorrow <wmcmorrow@gmail.com>
2012-08-18 19:48:46 -0400

    Printing forms from generic views

--- gungnir/projects/views.py
+++ gungnir/projects/views.py
@@ -1,4 +1,11 @@
 from django.views.generic import TemplateView
+from gungnir.projects.forms import *
 
+""" 
+ModelForms 
+"""
 class RepoView(TemplateView):
     template_name = "core/Repo.html"
+    
+    def get(self, request):
+        return self.render_to_response({'form':RepoForm})
