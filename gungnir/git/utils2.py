98e2459ca700a9d6a41c9af2c99b997e8e4f175d
Corey Bertram <cbertram@netflix.com>
2012-08-19 06:00:08 -0700

    new hotness

---
+++ gungnir/git/utils2.py
@@ -0,0 +1,30 @@
+import os
+from uuid import uuid4
+from fnmatch import fnmatch
+from git import Repo
+
+from django.conf import settings
+
+
+def iter_find_files(path, glob_pattern):
+    """Generator that yields files in a directory matching a given glob.
+        Will be used for finding settings and requirements files in a repo"""
+
+    for dirpath, dirnames, filenames in os.walk(path):
+
+        for filename in filenames:
+            if fnmatch(filename, glob_pattern):
+                yield os.path.join(dirpath, filename)
+
+def clone_repo(repo_url, local_path=None):
+
+    if local_path is None:
+        repo_root = getattr(settings, 'REPO_ROOT', '/tmp')
+        local_path = os.path.join(repo_root, uuid4())
+
+    repo =  Repo.clone_from(repo_url, local_path)
+
+    return repo
+
+
+
