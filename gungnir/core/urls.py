e1b8d28468914c8cd0d4003f2735035f1ad4eb64
Corey Bertram <cbertram@netflix.com>
2012-08-19 02:40:22 -0700

    login required sup

--- gungnir/core/urls.py
+++ gungnir/core/urls.py
@@ -1,18 +1,13 @@
 from django.conf.urls import patterns, url
 from django.core.urlresolvers import reverse_lazy
-
 from django.views.generic import TemplateView, RedirectView
+from django.contrib.auth.decorators import login_required
+
 from gungnir.core.views import ProfileView, DashboardView
 
 urlpatterns = patterns('gungnir.core.views',
     url(r'^$', RedirectView.as_view(url=reverse_lazy('gungnir-core-about')), name='gungnir-core-index'),
     url(r'^about/$', TemplateView.as_view(template_name='core/about.html'), name='gungnir-core-about'),
-    url(r'^profile/$', ProfileView.as_view(), name='profile'),
-    url(r'^dashboard/$', DashboardView.as_view(), name='gungnir-core-dashboard'),
+    url(r'^profile/$', login_required(ProfileView.as_view()), name='profile'),
+    url(r'^dashboard/$', login_required(DashboardView.as_view()), name='gungnir-core-dashboard'),
 )
-#urlpatterns = patterns('gungnir.core.views',
-#    url(r'^test/$', TemplateView.as_view(template_name='core/about.html') , 'core-index'),
-#)
-#
-#urlpatterns = patterns('django.contrib.flatpages.views',
-#    url(r'^about/$', 'flatpage', {'url': '/about/'}, name='about'),
