beae19543a0ca6b81cb2ab2f37cf5cec2ab4a61d
Corey Bertram <cbertram@netflix.com>
2012-08-19 05:55:29 -0700

    peace dash2 :

--- gungnir/core/urls.py
+++ gungnir/core/urls.py
@@ -9,7 +9,6 @@ urlpatterns = patterns('gungnir.core.views',
     url(r'^$', RedirectView.as_view(url=reverse_lazy('gungnir-core-about')), name='gungnir-core-index'),
     url(r'^about/$', TemplateView.as_view(template_name='core/about.html'), name='gungnir-core-about'),
     url(r'^profile/$', login_required(ProfileView.as_view()), name='profile'),
-    url(r'^dashboard/$', login_required(DashboardView.as_view()), name='gungnir-core-dashboard'),
-    url(r'^dashboard2/$', login_required(DashboardListView.as_view()), name='gungnir-core-dashboard2'),
+    url(r'^dashboard/$', login_required(DashboardListView.as_view()), name='gungnir-core-dashboard'),
 
 )
