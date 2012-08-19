2b505adf8ac2faa20a07a9c68c335054837139e9
jawnb <john@begeman.us>
2012-08-18 20:25:45 -0400

    typo

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,10 @@
 PROJECT_NAME = 'gungnir'
-PROJECT_ROOT = '/opt/venvs/gungnir/bin/site/badatcomputers'
+PROJECT_ROOT = '/opt/venvs/gungnir/sites/badatcomputers'
 PROJECT_SETTINGS = 'gungnir.prod_settings'
 
 VENV_PYTHON = '/opt/venvs/gungnir/bin/python'
 
-from fabric.api import run, sudo
+from fabric.api import run, sudo, cd
 
 def restart_gunicorn():
     sudo('/opt/venvs/gungnir/bin/supervisorctl -c /opt/venvs/gungnir/etc/supervisord.conf restart gungnir-gunicorn')
@@ -12,8 +12,8 @@ def restart_gunicorn():
 def git_update():
 
     with cd(PROJECT_ROOT):
-        run('git pull' )
-        run('{python} {project_root}/manage.py collectstatic --settings={settings}'.format(python=VENV_PYTHON, project_root=PROJECT_ROOT, settings=PROJECT_SETTINGS ))
+        sudo('git pull' )
+        sudo('{python} {project_root}/manage.py collectstatic --settings={settings}'.format(python=VENV_PYTHON, project_root=PROJECT_ROOT, settings=PROJECT_SETTINGS ))
 
 def deploy():
     git_update()
